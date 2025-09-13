28d550355f644206d90018607611c3d1d4427bab
Minhaj <minhajminhaj291@gmail.com>
2025-09-13 23:54:58 +0530

    fetched data successfully from API

---
+++ src/script.js
@@ -0,0 +1,45 @@
+const searchCityInput = document.querySelector('#search-city');
+const searchBtn = document.querySelector('#search-btn');
+const dateTimeEl = document.querySelector('#date-time');
+const cityEl = document.querySelector('#city');
+const temperatureEl = document.querySelector('#temperature');
+const descriptionEl = document.querySelector('#description');
+const tempImg = document.querySelector('#temp-img');
+const humiditySpan = document.querySelector('#humidity');
+const windSpan = document.querySelector('#wind');
+const feelsLikeSpan = document.querySelector('#feels');
+
+
+searchBtn.addEventListener('click', fetchWeatherInfo);
+searchCityInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    fetchWeatherInfo();
+  }
+})
+
+async function fetchWeatherInfo() {
+  const API_KEY = 'c5aaba02890c19af6920cdb6ec88c30f';
+  const cityName = searchCityInput.value.trim();
+
+  if (!cityName) {
+    alert('Enter the city name');
+    return
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+  // Fetching info from API
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("City not found");
+    const data = await response.json();
+    console.log(data);
+    renderInfo(data)
+  } catch (error) {
+    console.error("Error fetching weather:", error);
+  }
+}
+
+function renderInfo(data) {
+
+
+}
